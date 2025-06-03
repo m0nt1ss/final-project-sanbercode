68cd6e1d4c013f0d3b66912256b8a722da3d9951
m0nt1ss <147573634+m0nt1ss@users.noreply.github.com>
2025-06-03 10:24:35 +0700

    Delete sdn3_rejasari directory

--- sdn3_rejasari/buat_admin.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'koneksi.php';
-$pass = password_hash("admin123", PASSWORD_DEFAULT);
-mysqli_query($koneksi, "INSERT INTO admin (username, password) VALUES ('admin', '$pass')");
-echo "Admin berhasil dibuat.";
-?>
